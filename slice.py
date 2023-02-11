26d98f6c89c7cc7d300b95fe4ae829bf24afbca8
eksjedi <48331220+eksjedi@users.noreply.github.com>
2023-02-11 15:01:41 +0100

    Create slice.py
    jak dziłają slice w python

---
+++ slice.py
@@ -0,0 +1,6 @@
+digits = '0123456789'
+print(digits[0:3]) #wynik 012
+print(digits[:3])  #wynik 012
+print(digits[0:-1]) #wynik 012345678
+print(digits[0:])  #wynik 0123456789
+print(digits[::-1]) #wynik 9876543210
